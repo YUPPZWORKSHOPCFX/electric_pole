43342e56e3f3c6ee4e6f2a58de415714b596058c
YUPPZWORKSHOPCFX <129989756+YUPPZWORKSHOPCFX@users.noreply.github.com>
2023-05-02 18:36:38 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+--===== FiveM Script =========================================
+--= electric_props - YUPPZWORKSHOP CFX (Webhook)
+--===== Developed By: ========================================
+--= YUPPZWORKSHOP CFX
+--= Copyright (C) YUPPZWORKSHOP CFX - All Rights Reserved
+--= You are not allowed to sell this script or edit
+--============================================================
+
+fx_version "cerulean"
+game "gta5"
+
+description 'YUPPz WORKSHOP CFX'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/electric_pole01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/shot.ytyp'
+
